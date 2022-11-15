1484559ec76fcfd44407ccf7773612ddde54230e
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2022-11-15 14:11:47 +0100

    .gitignore added *jpg and *csv p2_main.py started to code first phase of the project

--- p2_main.py
+++ p2_main.py
@@ -1,16 +1,27 @@
 # import from pip
-import requests
-from bs4 import BeautifulSoup
 from art import *
-
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
 
 # welcome message
 tprint("OpenClassrooms", "white_bubble")
 tprint("P2_DA Python", "white_bubble")
+print("\n\n")
 
-# website to scrap
-url = "http://books.toscrape.com/"
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-# website response
 response = requests.get(url)
-print("Site Web en ligne ? :", response)
+
+if response.ok:
+    product_alone = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    product_page_url = url
+    universal_product_code = soup.findAll("tbody", "tr", "td")
+    title = soup.findAll("h1")
+    price_including_tax = soup.findAll("tbody", "tr", "td")
+    product_alone.append(price_including_tax)
+
+
+print(product_alone)
