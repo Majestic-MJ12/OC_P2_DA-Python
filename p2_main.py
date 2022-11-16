4872c832992de2f96626476e835e06f1e0f43ec3
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2022-11-16 10:11:22 +0100

    .gitignore added the folder data_extracted p2_main.py phase 1 almost finished, csv creation

--- p2_main.py
+++ p2_main.py
@@ -1,25 +1,35 @@
 # import from pip
+import csv
 from art import *
 from bs4 import BeautifulSoup
 import requests
-import re
 
 # welcome message
 tprint("OpenClassrooms", "white_bubble")
 tprint("P2_DA Python", "white_bubble")
-print("\n\n")
+print("\n")
 
 # website to scrap
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+
 # online?
+requests.get(url)
 response = requests.get(url)
+if response.status_code != 200:
+    print("bad URL")
+else:
+    print("The website to scrap is online:", response)
+
+# Get the html of the website into a variable
 html = response.content
 soup = BeautifulSoup(html, "html.parser")
+print("\n")
 
+# Variables with soup to extract information needed
 tr = soup.findAll("tr")
 p = soup.findAll("p")
 a = soup.findAll("a")
-
+# Get all needed information to create the csv file
 product_page_url = url
 print(product_page_url)
 title = soup.find("h1")
@@ -36,21 +46,16 @@ product_description = p[3].string
 print(product_description)
 category = a[3].string
 print(category)
-
-if re.match("star-rating One", html):
-    review_rating = 1
-elif re.search("star-rating Two", html):
-    review_rating = 2
-elif re.search("star-rating Three", html):
-    review_rating = 3
-elif re.search("star-rating Four", html):
-    review_rating = 4
-elif re.search("star-rating Five", html):
-    review_rating = 5
+review_rating = soup.find("p", {"class": "star-rating Three"})
+review_rating = review_rating["class"]
 print(review_rating)
+image_url = soup.find_all("img")
+image_url = image_url[0].get("src")
+value_url = ["http://books.toscrape.com/" + image_url]
+print(value_url)
 
-image_url = soup.find("img")
-print(image_url)
-
-
-
+# csv file creation
+csv.field_size_limit(10)
+header = ['product_page_url', 'upc', 'title', 'price_including_tax', "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
+writer = csv.writer(f)
+f.close()
