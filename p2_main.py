d7bc769c3f17e480a074472665df850d90990907
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2022-11-17 18:38:26 +0100

    p2_main.py: still can't make the phase 2 working

--- p2_main.py
+++ p2_main.py
@@ -6,6 +6,7 @@ import requests
 from art import *
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+
 # /import from pip
 
 # welcome message
@@ -24,15 +25,9 @@ if not os.path.exists(directory):
 print("\n")
 # /folder for the csv file creation
 
-# csv file information
-csv.field_size_limit(10)
-header = ['product_page_url', 'upc', 'title', 'price_including_tax', "price_excluding_tax", "number_available",
-          "product_description", "category", "review_rating", "image_url"]
-description = []
-# /csv file information
-
 # website to scrap
 url2 = "http://books.toscrape.com/catalogue/category/books/poetry_23/index.html"
+# /website to scrap
 requests.get(url2)
 response2 = requests.get(url2)
 if response2.status_code != 200:
@@ -41,13 +36,16 @@ else:
     print("The website to scrap is online:", response2)
     print("\n")
     # /online?
+# use of beautifulsoup
 soup2 = BeautifulSoup(response2.content, "html.parser")
-# /website to scrap
+# /use of beautifulsoup
+
+li_books = []
 
 
 # Get the list of the book from one category
 def get_url_books():
-    li_books = []
+    global li_books
     for link in soup2.find_all('h3'):
         li_books.append(urljoin("http://books.toscrape.com/catalogue/category/books/poetry_23/index.html",
                                 link.find("a").get("href")))
@@ -57,21 +55,17 @@ def get_url_books():
 
 get_url_books()
 
+description = []
 
-books_url = get_url_books()
 
 # website to scrap
-for books in books_url:
+
+for books in li_books:
     url = books
     # /website to scrap
     # online url?
     requests.get(url)
     response = requests.get(url)
-    if response.status_code != 200:
-        print("bad URL")
-    else:
-        print("The website to scrap is online:", response)
-        print("\n")
     # use of beautifulsoup
     soup = BeautifulSoup(response.content, "html.parser")
     # /use of beautifulsoup
@@ -83,70 +77,66 @@ for books in books_url:
     # /Variables with soup to extract information needed
 
     # get all needed information to create the csv file
-    def des():
-        product_page_url = url
-        description.append(product_page_url)
 
-        title = soup.find("h1").string
-        description.append(title)
+    product_page_url = url
+    description.append(product_page_url)
 
-        upc = tr[0].td.string
-        description.append(upc)
+    title = soup.find("h1").string
+    description.append(title)
 
-        price_including_tax = tr[2].td.string
-        price_including_tax = re.sub('[^0-9.]+', '', price_including_tax)
-        description.append(price_including_tax)
+    upc = tr[0].td.string
+    description.append(upc)
 
-        price_excluding_tax = tr[3].td.string
-        price_excluding_tax = re.sub('[^0-9.]+', '', price_excluding_tax)
-        description.append(price_excluding_tax)
+    price_including_tax = tr[2].td.string
+    price_including_tax = re.sub('[^0-9.]+', '', price_including_tax)
+    description.append(price_including_tax)
 
-        number_available = tr[5].td.string
-        description.append(number_available.string)
+    price_excluding_tax = tr[3].td.string
+    price_excluding_tax = re.sub('[^0-9.]+', '', price_excluding_tax)
+    description.append(price_excluding_tax)
 
-        product_description = p[3].string
-        description.append(product_description)
+    number_available = tr[5].td.string
+    description.append(number_available)
 
-        category = a[3].string
-        description.append(category)
+    product_description = p[3].string
+    description.append(product_description)
 
-        review_rating = soup.find("p", {"class": "star-rating"})
-        review_rating = review_rating["class"]
-        if "One" in review_rating:
-            review_rating = 1
-        elif "Two" in review_rating:
-            review_rating = 2
-        elif "Three" in review_rating:
-            review_rating = 3
-        elif "Four" in review_rating:
-            review_rating = 4
-        elif "Five" in review_rating:
-            review_rating = 5
-        description.append(review_rating)
+    category = a[3].string
+    description.append(category)
 
-        image_url = soup.find_all("img")
-        image_url = image_url[0].get("src")
-        value_url = "http://books.toscrape.com/" + image_url
-        description.append(value_url)
-        # /get all needed information to create the csv file
+    review_rating = soup.find("p", {"class": "star-rating"})
+    review_rating = review_rating["class"]
+    if "One" in review_rating:
+        review_rating = "1"
+    elif "Two" in review_rating:
+        review_rating = "2"
+    elif "Three" in review_rating:
+        review_rating = "3"
+    elif "Four" in review_rating:
+        review_rating = "4"
+    elif "Five" in review_rating:
+        review_rating = "5"
+    description.append(review_rating)
 
-
-    des()
-
+    image_url = soup.find_all("img")
+    image_url = image_url[0].get("src")
+    value_url = "http://books.toscrape.com/" + image_url
+    description.append(value_url)
+    # /get all needed information to create the csv file
+    print(description)
 
 # create the csv file with the headers and the descriptions
-def csv_creation():
-    with open("data_extracted/data.csv", "w", encoding='UTF8', newline='') as data_csv:
-        writer = csv.writer(data_csv, delimiter=";")
-        # gives the header name row into csv
-        writer.writerow(header)
-        # data add in csv file
-        writer.writerow(description)
-        if True:
-            print("All the data has been scrapped in the folder: data_extracted")
-            print("The file name is : data.csv")
-            print("You can open the file with a csv software now")
+with open("data_extracted/data.csv", "w", encoding='UTF8', newline='') as data_csv:
+    writer = csv.writer(data_csv, delimiter=";")
+    # gives the header name row into csv
+    writer.writerow(['product_page_url', 'title', 'upc', 'price_including_tax', "price_excluding_tax",
+                     "number_available",
+                     "product_description", "category", "review_rating", "image_url"])
+    # data add in csv file
+    writer.writerow(description)
 
-
-csv_creation()
+    if True:
+        print("All the data has been scrapped in the folder: data_extracted")
+        print("The file name is : data.csv")
+        print("You can open the file with a csv software now")
 # /create the csv file with the headers and the descriptions
