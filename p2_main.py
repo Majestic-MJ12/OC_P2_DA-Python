21b91f860c5251683fdb525bbe24f393d7863543
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2022-11-18 19:56:54 +0100

    p2_main.py: phase 3 creation of all the csv over

--- p2_main.py
+++ p2_main.py
@@ -6,6 +6,7 @@ import requests
 from art import *
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import time
 # /import package
 
 # welcome message
@@ -42,6 +43,7 @@ soup3 = BeautifulSoup(response3.content, "html.parser")
 
 # list of all categories url
 li_category = []
+num_cat = 1
 # / list of all categories url
 
 
@@ -49,7 +51,7 @@ li_category = []
 def get_url_category():
     global li_category
     for cat in soup3.findAll("a"):
-        li_category.append(urljoin("https://books.toscrape.com/index.html".rstrip(), cat.get("href")))
+        li_category.append(urljoin(url3.rstrip(), cat.get("href")))
     return li_category
 
 
@@ -58,134 +60,134 @@ get_url_category()
 
 # remove the two first useless url from the categories url list
 del li_category[0:3]
-del li_category[50:120]
-print(li_category)
+del li_category[50:100]
 # / remove the two first useless url from the categories url list
 
+for category in li_category:
+    url2 = category
+    # /website to scrap
+    # online url?
+    requests.get(url2)
+    response2 = requests.get(url2)
+    # use of beautifulsoup
+    soup2 = BeautifulSoup(response2.content, "html.parser")
+    # /use of beautifulsoup
 
-# get soup from one category url
-url2 = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
-requests.get(url2)
-response2 = requests.get(url2)
-# use of beautifulsoup
-soup2 = BeautifulSoup(response2.content, "html.parser")
-# /use of beautifulsoup
-# / get soup from one category url
+    # list of all url on one category's page
+    books_category = []
+    # list of all url on one category's page
 
-# list of all url on one category's page
-books_category = []
-# list of all url on one category's page
+    # buckle to fill the books_category
+    while True:
+        li_books = []
 
-# buckle to fill the books_category
-while True:
-    li_books = []
+        # Get the list of the book from one category
+        def get_url_books():
+            global li_books
+            for link in soup2.find_all('h3'):
+                li_books.append(urljoin(url2,
+                                        link.find("a").get("href")))
+            return li_books
+        # /Get the list of the book from one category
 
-    # Get the list of the book from one category
-    def get_url_books():
-        global li_books
-        for link in soup2.find_all('h3'):
-            li_books.append(urljoin("https://books.toscrape.com/catalogue/category/books/travel_2/index.html",
-                                    link.find("a").get("href")))
-        return li_books
-    # /Get the list of the book from one category
+        get_url_books()
 
-    get_url_books()
+        # website to scrap
+        for books in li_books:
+            url = books
+            # /website to scrap
+            # online url?
+            requests.get(url)
+            response = requests.get(url)
+            # use of beautifulsoup
+            soup = BeautifulSoup(response.content, "html.parser")
+            # /use of beautifulsoup
 
-    # website to scrap
-    for books in li_books:
-        url = books
-        # /website to scrap
-        # online url?
-        requests.get(url)
-        response = requests.get(url)
-        # use of beautifulsoup
-        soup = BeautifulSoup(response.content, "html.parser")
-        # /use of beautifulsoup
+            # Variables with soup to extract information needed
+            tr = soup.findAll("tr")
+            p = soup.findAll("p")
+            a = soup.findAll("a")
+            # /Variables with soup to extract information needed
 
-        # Variables with soup to extract information needed
-        tr = soup.findAll("tr")
-        p = soup.findAll("p")
-        a = soup.findAll("a")
-        # /Variables with soup to extract information needed
+            # get all needed information to create the csv file
+            description = []
 
-        # get all needed information to create the csv file
-        description = []
+            product_page_url = url
+            description.append(product_page_url)
 
-        product_page_url = url
-        description.append(product_page_url)
+            title = soup.find("h1").string
+            description.append(title)
 
-        title = soup.find("h1").string
-        description.append(title)
+            upc = tr[0].td.string
+            description.append(upc)
 
-        upc = tr[0].td.string
-        description.append(upc)
+            price_including_tax = tr[2].td.string
+            price_including_tax = re.sub('[^0-9.]+', '', price_including_tax)
+            description.append(price_including_tax)
 
-        price_including_tax = tr[2].td.string
-        price_including_tax = re.sub('[^0-9.]+', '', price_including_tax)
-        description.append(price_including_tax)
+            price_excluding_tax = tr[3].td.string
+            price_excluding_tax = re.sub('[^0-9.]+', '', price_excluding_tax)
+            description.append(price_excluding_tax)
 
-        price_excluding_tax = tr[3].td.string
-        price_excluding_tax = re.sub('[^0-9.]+', '', price_excluding_tax)
-        description.append(price_excluding_tax)
+            number_available = tr[5].td.string
+            description.append(number_available)
 
-        number_available = tr[5].td.string
-        description.append(number_available)
+            product_description = p[3].string
+            description.append(product_description)
 
-        product_description = p[3].string
-        description.append(product_description)
+            category = a[3].string
+            description.append(category)
 
-        category = a[3].string
-        description.append(category)
+            review_rating = soup.find("p", {"class": "star-rating"})
+            review_rating = review_rating["class"]
+            if "One" in review_rating:
+                review_rating = "1"
+            elif "Two" in review_rating:
+                review_rating = "2"
+            elif "Three" in review_rating:
+                review_rating = "3"
+            elif "Four" in review_rating:
+                review_rating = "4"
+            elif "Five" in review_rating:
+                review_rating = "5"
+            description.append(review_rating)
 
-        review_rating = soup.find("p", {"class": "star-rating"})
-        review_rating = review_rating["class"]
-        if "One" in review_rating:
-            review_rating = "1"
-        elif "Two" in review_rating:
-            review_rating = "2"
-        elif "Three" in review_rating:
-            review_rating = "3"
-        elif "Four" in review_rating:
-            review_rating = "4"
-        elif "Five" in review_rating:
-            review_rating = "5"
-        description.append(review_rating)
+            image_url = soup.find_all("img")
+            image_url = image_url[0].get("src")
+            value_url = "http://books.toscrape.com/" + image_url
+            description.append(value_url)
 
-        image_url = soup.find_all("img")
-        image_url = image_url[0].get("src")
-        value_url = "http://books.toscrape.com/" + image_url
-        description.append(value_url)
+            books_category.append(description)
+            # /get all needed information to create the csv file
 
-        books_category.append(description)
-        # /get all needed information to create the csv file
+        next_page = soup2.find("li", {"class": "next"})
+        if next_page is not None:
+            next_page = next_page["class"]
+            li = soup2.find("li", {"class": "next"})
+            url2 = urljoin(url2.rstrip(),
+                           li.find("a").get("href"))
+            requests.get(url2)
+            response2 = requests.get(url2)
+            # use of beautifulsoup
+            soup2 = BeautifulSoup(response2.content, "html.parser")
+            # /use of beautifulsoup
+        else:
+            break
+    # buckle to fill the books_category
 
-    next_page = soup2.find("li", {"class": "next"})
-    if next_page is not None:
-        next_page = next_page["class"]
-        li = soup2.find("li", {"class": "next"})
-        url2 = urljoin("https://books.toscrape.com/catalogue/category/books/travel_2/index.html".rstrip(),
-                       li.find("a").get("href"))
-        requests.get(url2)
-        response2 = requests.get(url2)
-        # use of beautifulsoup
-        soup2 = BeautifulSoup(response2.content, "html.parser")
-        # /use of beautifulsoup
-    else:
-        break
-# buckle to fill the books_category
+    # create the csv file with the headers and the descriptions
+    csv_name = "data_extracted/" + time.strftime("%Y-%m-%d") + "_category_" + str(num_cat) + ".csv"
+    with open(csv_name, "w", encoding='UTF8', newline='') as data_csv:
+        writer = csv.writer(data_csv, delimiter=";")
+        # gives the header name row into csv
+        writer.writerow(['product_page_url', 'title', 'upc', 'price_including_tax', "price_excluding_tax",
+                         "number_available",
+                         "product_description", "category", "review_rating", "image_url"])
+        # data add in csv file
+        writer.writerows(books_category)
 
-# create the csv file with the headers and the descriptions
-with open("data_extracted/data.csv", "w", encoding='UTF8', newline='') as data_csv:
-    writer = csv.writer(data_csv, delimiter=";")
-    # gives the header name row into csv
-    writer.writerow(['product_page_url', 'title', 'upc', 'price_including_tax', "price_excluding_tax",
-                     "number_available",
-                     "product_description", "category", "review_rating", "image_url"])
-    # data add in csv file
-    writer.writerows(books_category)
-
-    if True:
-        print("All the data has been scrapped in the folder: data_extracted")
-        print("The file name is : data.csv")
-        print("You can open the file with a csv software now")
-# /create the csv file with the headers and the descriptions
+        if True:
+            print("The file name is :", csv_name)
+            print("You can open the file with a csv software now")
+    # /create the csv file with the headers and the descriptions
+    num_cat = num_cat + 1
